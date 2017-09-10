e060fa8569fb637690a070a4dcb915e8d83a47f2
CChanggg <373893135@qq.com>
2017-09-10 20:42:02 +0800

    demo

---
+++ simple-app-demo/app.js
@@ -0,0 +1,9 @@
+// 前端开发因node 带来了命令行的编译环境 产生了大前端 
+// development product
+// 工作流进行重构 高效 npm开源
+// 模块机制 项目工程化 版本管理
+// 对外发布 一键 npm run build 
+// 持续集成的标准开发流程 vue-cli
+// node开发前端脚本的好处：模块化机制，版本管理，对外发布，持续集成标准的开发流程
+const $ = require('jQuery')
+$('h1').css({ color: 'red' })
